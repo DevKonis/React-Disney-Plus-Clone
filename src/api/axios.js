ad00d3e40d74498fa63b76578c2911b5c296d1a7
DevKonis <118440358+DevKonis@users.noreply.github.com>
2023-08-22 19:36:36 +0900

    update 11 files

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
+// const config = {
+//   headers: {
+//     "Access-Control-Allow-Origin": "*",
+//     "Content-Type": "text/plain",
+//   },
+// };
+
 const instance = axios.create({
-  baseURL: "http://api.themoviesdb.org/3",
+  baseURL: "http://api.themoviedb.org/3",
+  // https: config,
   params: {
     api_key: "923c2f2c832ee1662f24164cf921918c",
-    language: "ko-KR"
-  }
-})
+    language: "ko-KR",
+  },
+});
 
 export default instance;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,124 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
+import requests from "../api/request";
+import "./Banner.css";
+import styled from "styled-components";
 
 const Banner = () => {
-  return (
-    <div>Banner</div>
-  )
-}
+  const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
-export default Banner
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get(requests.fetchNowPlaying);
+    const movieId =
+      response.data.results[
+        Math.floor(Math.random() * response.data.results.length)
+      ].id;
+
+    const { data: movieDetails } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
+
+    setMovie(movieDetails);
+  };
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  };
+
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/Ke_P8Ce4e1E"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+  ></iframe>;
+
+  if (isClicked) {
+    return (
+      <React.Fragment>
+        <Container>
+          <HomeContainer>
+            <Iframe
+              src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+              width="640"
+              height="360"
+              frameborder="0"
+              allow="autoplay; fullscreen"
+            ></Iframe>
+          </HomeContainer>
+        </Container>
+        <button onClick={() => setIsClicked(false)}>X</button>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPostion: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+
+          <div className="banner__buttons">
+            {movie?.videos?.results[0]?.key && (
+              <button
+                className="banner__button play"
+                onClick={() => setIsClicked(true)}
+              >
+                play
+              </button>
+            )}
+          </div>
+          <p className="banner__description">{truncate(movie.overview)}</p>
+        </div>
+        <div className="banner--fadeBottom" />
+      </header>
+    );
+  }
+};
+
+export default Banner;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
